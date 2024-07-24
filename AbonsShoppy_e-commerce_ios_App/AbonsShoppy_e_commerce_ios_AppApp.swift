e6d9e2ef795359c5f68733782ddd5918938a40e7
Ezgi Karahan <karahanezgi64@gmail.com>
2024-07-24 20:35:32 +0300

    Initial Commit

---
+++ AbonsShoppy_e-commerce_ios_App/AbonsShoppy_e_commerce_ios_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbonsShoppy_e_commerce_ios_AppApp.swift
+//  AbonsShoppy_e-commerce_ios_App
+//
+//  Created by Ezgi Karahan on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AbonsShoppy_e_commerce_ios_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbonsShoppy_e-commerce_ios_App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AbonsShoppy_e-commerce_ios_App
+//
+//  Created by Ezgi Karahan on 24.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
