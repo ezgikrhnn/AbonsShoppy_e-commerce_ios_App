99920e620c045de936e84f74c7da03a3d8393ba0
Ezgi Karahan <karahanezgi64@gmail.com>
2024-08-02 15:31:00 +0300

    Network manager updated.

--- AbonsShoppy_e-commerce_ios_App/Extensions/NetworkManager.swift
+++ AbonsShoppy_e-commerce_ios_App/Extensions/NetworkManager.swift
@@ -8,26 +8,45 @@
 //url session ve json decoder kullanıldı. urlSession ve jsonserialization da kullanılabilirdi.
 import Foundation
 
-class NetworkManager: ObservableObject {
-    @Published var products: [Product] = []
+class NetworkManager {
     
-    func fetchProducts() {
-        guard let url = URL(string: "https://fakestoreapi.com/products") else {
-            print("url hatası")
+   static let shared = NetworkManager() //static yapmazsan çalışmaz
+    private init() { }  //shared singletone'unu tanımladıgım için init lazım.
+
+    //fetchData func:
+    /**Bu fonksiyon verilen endpointten veri çekmek ve bu veriyi belirtilen türde çözümlemek için kullanılır.**/
+    func fetchData<T: Codable>(from endpoint: Endpoint, expecting type: T.Type, completion: @escaping (Result<T, Error>)-> Void) {
+         
+        // 1- url al:
+        let baseUrl = "https://fakestoreapi.com"
+        guard let url = URL(string: "\(baseUrl)/\(endpoint.url)") else { print("geçersiz url")
             return }
-       
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                do {
-                    let decodedData = try JSONDecoder().decode([Product].self, from: data)
-                    DispatchQueue.main.async {
-                        self.products = decodedData
-                        
-                    }
-                } catch {
-                    print("Error decoding data: \(error)")
-                }
+        
+        // 2- URLSession ile istek:
+        let task = URLSession.shared.dataTask(with:url) { data, response, error in
+             
+            // 3 - urlsession içinde error kontrolü
+            if let error = error {
+                completion(.failure(error))
+                return
             }
-        }.resume()
+            
+            // 4 - urlsession içi data kontrolu
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No Data", code: 0,  userInfo: nil)))
+                return
+            }
+            
+            // 5 - veriyi jsondecoder ile al: do try catch
+            do{
+                let decodedData = try JSONDecoder().decode(type, from: data)
+                completion(.success(decodedData))
+            }catch{
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+        
     }
+    
 }
